32f5f94223f8349427a547b1b08a2e84928865b3
epraem <efraimg@n-compass.biz>
2026-05-25 13:41:46 +0800

    Revert "- non indexable fix"
    This reverts commit ae67771a2ee4ed3a02a339501531e780ca61dd11.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marquee-hub-template.vercel.app',
+  site: 'https://www.ntv360.space',
   output: 'static',
   adapter: vercel(),
   
