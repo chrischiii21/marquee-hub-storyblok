ae67771a2ee4ed3a02a339501531e780ca61dd11
epraem <efraimg@n-compass.biz>
2026-05-25 13:31:17 +0800

    - non indexable fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ntv360.space',
+  site: 'https://marquee-hub-template.vercel.app',
   output: 'static',
   adapter: vercel(),
   
