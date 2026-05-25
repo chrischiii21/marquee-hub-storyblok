fa800fb06285738d5df3d8325dd045d06253ead4
epraem <efraimg@n-compass.biz>
2026-05-25 13:38:39 +0800

    - non indexable

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://marquee-hub-template.vercel.app',
   output: 'static',
+  trailingSlash: 'always',
   adapter: vercel(),
   
   vite: {
